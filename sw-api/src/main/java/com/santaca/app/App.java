f1a759102965763b1c161fd98323e979988b887b
Santaca <38440843+Santaca@users.noreply.github.com>
2024-09-06 10:50:34 +0200

    Develop (#1)
    * Java added
    
    * java added
    
    * Python script updated
    
    * Hpa, ingress and service added
    
    * Java classes create
    
    * order_json_array implemented
    
    * HttpServer hostname changed
    
    * docker-compose.yml added

---
+++ sw-api/src/main/java/com/santaca/app/App.java
@@ -0,0 +1,21 @@
+package com.santaca.app;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", 9090), 0);
+            server.createContext("/", new MyHttpHandler());
+            server.setExecutor(null);
+            server.start();
+            System.out.println("Servidor corriendo en purto 9090");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ sw-api/src/main/java/com/santaca/app/MyHttpHandler.java
@@ -0,0 +1,54 @@
+package com.santaca.app;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class MyHttpHandler implements  HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        if (exchange.getRequestMethod().equals("GET")) {
+            System.out.println("Dentro del IF");
+            String param = exchange.getRequestURI().toString();
+            MakeRequest req = new MakeRequest(param);
+            String result = req.call_api();
+
+            JSONObject jsonfy = new JSONObject(result);
+            System.out.println(jsonfy.length());
+
+            String results_aux = jsonfy.get("results").toString();
+            JSONArray arr = new JSONArray(results_aux);
+            
+            JSONArray sorted_arr = order_json_array(arr);
+
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, results_aux.getBytes().length);
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(results_aux.getBytes());
+            os.close();
+
+        } else {
+            String error = "Method Not Allowed";
+            exchange.getResponseHeaders().set("Content-Type", "application/text");
+            exchange.sendResponseHeaders(405, error.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(error.getBytes());
+            os.close();
+            throw new UnsupportedOperationException("Unimplemented method 'handle'");
+        }
+    }
+    
+    private JSONArray order_json_array(JSONArray arr) {
+        System.out.println("Ordenamos el array");
+        System.out.println(arr.opt(0));
+        return null;
+    }
+    
+}
